d9950e827a763624a843673f67533bc9de1790ca
animation <763678968@qq.com>
2019-08-19 15:48:24 +0800

    策略模式

--- src/StrategyPattern/Quack.java
+++ src/StrategyPattern/Quack.java
@@ -1,6 +1,8 @@
+package StrategyPattern;
+
 public class Quack implements QuackBehavior {
     @Override
     public void quack() {
-        System.out.println("Quack");
+        System.out.println("StrategyPattern.Quack");
     }
 }
