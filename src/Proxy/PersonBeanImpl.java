46ef07a8c673258fce8206be9211ab953728e1d6
animation <763678968@qq.com>
2019-08-25 21:20:44 +0800

    代理模式 配对服务 PersonBeanImpl

---
+++ src/Proxy/PersonBeanImpl.java
@@ -0,0 +1,50 @@
+package Proxy;
+
+public class PersonBeanImpl implements PersonBean {
+    String name;
+    String gender;
+    String interests;
+    int rating;
+    int ratingCount = 0;
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getGender() {
+        return gender;
+    }
+
+    @Override
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public String getInterests() {
+        return interests;
+    }
+
+    public void setInterests(String interests) {
+        this.interests = interests;
+    }
+
+    public int getHotOrNotRating() {
+        if (ratingCount == 0) return 0;
+        return (rating/ratingCount);
+    }
+
+    public void setHotOrNotRating(int rating) {
+        this.rating += rating;
+        ratingCount++;
+    }
+
+
+}
