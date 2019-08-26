59e34628cbb72c8fa2a7e5a32dfd619a641a74e0
animation <763678968@qq.com>
2019-08-26 15:13:39 +0800

    组合模式 GooseAdapter

---
+++ src/Composite/GooseAdapter.java
@@ -0,0 +1,13 @@
+package Composite;
+
+public class GooseAdapter  implements Quackable {
+    Goose goose;
+
+    public GooseAdapter(Goose goose) {
+        this.goose = goose;
+    }
+
+    public void quack() {
+        goose.honk();
+    }
+}
