04c32b4c76c72db3c0373a40ed0605b7dfe0701c
animation <763678968@qq.com>
2019-10-28 11:19:37 +0800

    简单工厂

--- src/Test/Client.java
+++ src/Test/Client.java
@@ -3,15 +3,8 @@ package Test;
 public class Client {
 
     public static void main(String[] args) {
-        int type = 1;
-        Product product;
-        if (type == 1) {
-            product = new ConcreteProduct1();
-        } else if (type == 2) {
-            product = new ConcreteProduct2();
-        } else {
-            product = new ConcreteProduct();
-        }
+        SimpleFactory simpleFactory = new SimpleFactory();
+        Product product = simpleFactory.createProduct(1);
         // do something with the product
     }
 }
